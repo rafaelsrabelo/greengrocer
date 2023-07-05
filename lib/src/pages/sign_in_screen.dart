65dbebb70944821d951b53d5ae6a6dd7bff44a60
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-07-04 22:08:55 -0300

    signin

--- lib/src/pages/sign_in_screen.dart
+++ lib/src/pages/sign_in_screen.dart
@@ -6,140 +6,149 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Column(
-        children: [
-          Expanded(
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text.rich(
-                  TextSpan(
-                    style: TextStyle(
-                      fontSize: 40,
-                    ),
-                    children: [
+      backgroundColor: Colors.red,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              Expanded(
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text.rich(
                       TextSpan(
-                        text: 'Dindin',
                         style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold
-                        )
-                      ),
-                      TextSpan(
-                        text: 'da',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold
-
-                        )
-                      ),
-                      TextSpan(
-                        text: 'leia',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold
-
-                        )
+                          fontSize: 40,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: '@dindin.',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+                            )
+                          ),
+                          TextSpan(
+                            text: 'da',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+              
+                            )
+                          ),
+                          TextSpan(
+                            text: 'leia',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+              
+                            )
+                          )
+                        ]
                       )
-                    ]
-                  )
+                    ),
+                    const Text('Faça seu pedido',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+                    ),
+                    )
+                  ],
                 ),
-                const Text('Delivery',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.bold,
-                ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(45),
-                )),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const CustomTextiField(
-                  icon: Icons.email,
-                  label: 'Email',
-                ),
-                const CustomTextiField(
-                  icon: Icons.password,
-                  label: 'Senha',
-                  isSecret: true,
-                ),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18))),
-                      onPressed: () {},
-                      child: const Text(
-                        'Entrar',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      )),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Esqueceu a senha?',
-                        style: TextStyle(color: Colors.red),
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey[200],
-                          thickness: 2,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Text('ou'),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey[200],
-                          thickness: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: 50,
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18),
-                      ),
-                      side: BorderSide(
-                        width: 2,
-                        color: Colors.blue,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(45),
+                    )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const CustomTextiField(
+                      icon: Icons.email,
+                      label: 'Email',
+                    ),
+                    const CustomTextiField(
+                      icon: Icons.password,
+                      label: 'Senha',
+                      isSecret: true,
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18))),
+                          onPressed: () {},
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          )),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Esqueceu a senha?',
+                            style: TextStyle(color: Colors.red),
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey[200],
+                              thickness: 2,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text('ou'),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey[200],
+                              thickness: 2,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    onPressed: () {}, 
-                    child: const Text('Criar conta',
-                      style: TextStyle(fontSize: 18, color: Colors.blue),
-                    )),
+                    SizedBox(
+                      width: 50,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          side: BorderSide(
+                            width: 2,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        onPressed: () {}, 
+                        child: const Text('Criar conta',
+                          style: TextStyle(fontSize: 18, color: Colors.blue),
+                        )),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
